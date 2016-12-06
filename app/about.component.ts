c415ae68d995e1447779e2d90c577cc2be9d11ae
Марат Хабибуллин <khabibullin@hightech.today>
2016-12-06 19:16:13 +0300

    Route

---
+++ app/about.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'about-app',
+    template: `<h3>О сайте</h3>`
+})
+export class AboutComponent {
+    
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,25 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+
+import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent }   from './app.component';
-import { ChildComponent } from './child.component'
+import { ChildComponent } from './child.component';
+import { AboutComponent } from './about.component';
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
+
+// Определение маршрутов http://metanit.com/web/angular2/7.1.php
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', component: NotFoundComponent }
+]; 
 
 @NgModule({
-    imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent, ChildComponent ],
+    imports:      [ BrowserModule, FormsModule, RouterModule.forRoot(appRoutes) ],
+    declarations: [ AppComponent, ChildComponent, HomeComponent, AboutComponent, NotFoundComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }

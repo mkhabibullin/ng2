edec6562c032b73e891264982152adccccd12ea4
Марат Хабибуллин <khabibullin@hightech.today>
2016-12-09 17:52:24 +0300

    Component events

--- app/Binding/TwoWay/child.component.ts
+++ app/Binding/TwoWay/child.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter ,
+    OnInit,
+         DoCheck,
+         OnChanges,
+        AfterContentInit, 
+        AfterContentChecked, 
+        AfterViewChecked, 
+        AfterViewInit} from '@angular/core';
 
 @Component({
     selector: 'binding-twoway-child-comp',
@@ -7,12 +14,53 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
         <input [ngModel]="userName" (ngModelChange)="onNameChange($event)" />
         </div>`
 })
-export class ChildComponent {
+export class ChildComponent implements OnInit,
+         DoCheck,
+         OnChanges,
+        AfterContentInit, 
+        AfterContentChecked, 
+        AfterViewChecked, 
+        AfterViewInit {
     @Input() userName: string;
     @Output() userNameChange = new EventEmitter<string>();
+    count:number=1;
 
     onNameChange(model: string){
         this.userName = model;
         this.userNameChange.emit(model);
+    }
+
+    ngOnInit() {
+       
+      this.log(`ngOnInit`);
+    }
+    ngOnChanges() {
+       
+      this.log(`OnChanges`);
+    }
+    ngDoCheck() {
+       
+      this.log(`ngDoCheck`);
+    }
+    ngAfterViewInit() {
+       
+      this.log(`ngAfterViewInit`);
+    }
+    ngAfterViewChecked() {
+       
+      this.log(`ngAfterViewChecked`);
+    }
+    ngAfterContentInit() {
+       
+      this.log(`ngAfterContentInit`);
+    }
+    ngAfterContentChecked() {
+       
+      this.log(`ngAfterContentChecked`);
+    }
+ 
+    private log(msg: string) {
+        console.log(this.count + ". " + msg);
+        this.count++;
     }
 }
